4ef617aead8495de93f94f3618c81d682444f8fe
Robson Ronzani <ronzani.robson@gmail.com>
2019-02-02 10:56:06 -0300

    Alterado para transferir arquivo

--- Cliente.java
+++ Cliente.java
@@ -3,20 +3,31 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 import java.net.Socket;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class Cliente {
     public static void main(String[] args) throws UnknownHostException, IOException {
+        
+        File f = new File("/home/robson/pos/tr_pd_01/arquivo.txt");
+        FileInputStream in = new FileInputStream(f);
+
         Socket cliente = new Socket("127.0.0.1", 12345);
         System.out.println("O cliente se conectou ao servidor!");
 
-        Scanner teclado = new Scanner(System.in);
-        PrintStream saida = new PrintStream(cliente.getOutputStream());
-
-        while (teclado.hasNextLine()) {
-            saida.println(teclado.nextLine());
-        }
-
-        saida.close();
-        teclado.close();
+        OutputStream out = cliente.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(out);
+        BufferedWriter writer = new BufferedWriter(osw);
+        writer.write(f.getName()+"\n");
+        writer.flush();
+        int tamanho = 4096; // buffer de 4KB  
+        byte[] buffer = new byte[tamanho];  
+        int lidos = -1;  
+        while ((lidos = in.read(buffer, 0, tamanho)) != -1) {  
+            out.write(buffer, 0, lidos);  
+        }  
     }
 }
--- Servidor.java
+++ Servidor.java
@@ -3,20 +3,36 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.net.Socket;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Servidor {
   public static void main(String[] args) throws IOException {
-    ServerSocket servidor = new ServerSocket(12345);
-    System.out.println("Porta 12345 aberta!");
+    try {
+      ServerSocket server = new ServerSocket(12345);
+      System.out.println("Porta 12345 aberta!");
 
-    Socket cliente = servidor.accept();
-    System.out.println("Nova conexao com o cliente " + cliente.getInetAddress().getHostAddress());
-
-    Scanner entrada = new Scanner(cliente.getInputStream());
-    while(entrada.hasNextLine()) {
-      System.out.println("Mensagem recebida: " + entrada.nextLine());
+      Socket clSocket = server.accept();
+      InputStream in = clSocket.getInputStream();
+      InputStreamReader isr = new InputStreamReader(in);
+      BufferedReader reader = new BufferedReader(isr);
+      // String fName = reader.readLine();
+      // System.out.println(fName);
+      File f1 = new File("/home/robson/pos/tr_pd_01/arquivo_transferido.txt");
+      FileOutputStream out = new FileOutputStream(f1);
+          int tamanho = 4096; // buffer de 4KB  
+          byte[] buffer = new byte[tamanho];  
+          int lidos = -1;  
+          while ((lidos = in.read(buffer, 0, tamanho)) != -1) {  
+            System.out.println(lidos);
+              out.write(buffer, 0, lidos);  
+          }  
+          out.flush();  
+    } catch (IOException e) {
     }
-
-    entrada.close();
-    servidor.close();
+    
   }
 }
